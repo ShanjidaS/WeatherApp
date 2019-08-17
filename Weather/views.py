a6c4cf9b21008d5db314eadbfc5f7befb3fa4afa
Shanjida Swaranika <sswara01@mail.dcs.bbk.ac.uk>
2019-08-17 16:47:03 +0100

    Getting weather for a single city

--- Weather/views.py
+++ Weather/views.py
@@ -1,4 +1,18 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'Weather/Weather.html')
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=8d8eb521cfe3ef7d22cc093b1de2f841'
+    city = 'Amsterdam'
+
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    context = {'city_weather' : city_weather}
+    return render(request, 'Weather/Weather.html', context)
