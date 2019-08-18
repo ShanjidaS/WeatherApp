11f5750971f2087c5dc9294d625492f6fbbf8477
Shanjida Swaranika <sswara01@mail.dcs.bbk.ac.uk>
2019-08-18 22:02:36 +0100

    Created City model to add multiple cities

--- Weather/views.py
+++ Weather/views.py
@@ -1,18 +1,32 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=8d8eb521cfe3ef7d22cc093b1de2f841'
-    city = 'Amsterdam'
+    city = 'London'
+
+    cities = City.objects.all()
+
+    weather_data = []
 
     r = requests.get(url.format(city)).json()
 
-    city_weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
+    for city in cities:
+
+        city_weather = {
+            'city' : city,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+            }
+
+        weather_data.append(city_weather)
+
+    print(weather_data)
+
+    context = {
+        'weather_data' : weather_data,
     }
 
-    context = {'city_weather' : city_weather}
     return render(request, 'Weather/Weather.html', context)
