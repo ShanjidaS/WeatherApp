9ae1715ac9768072a3aa404cf31ff39c667b77a3
Shanjida Swaranika <sswara01@mail.dcs.bbk.ac.uk>
2019-08-19 00:39:48 +0100

    Added button functionality to allow user to add cities

--- Weather/views.py
+++ Weather/views.py
@@ -1,10 +1,17 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=8d8eb521cfe3ef7d22cc093b1de2f841'
     city = 'London'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -27,6 +34,7 @@ def index(request):
 
     context = {
         'weather_data' : weather_data,
+        'form' : form
     }
 
     return render(request, 'Weather/Weather.html', context)
